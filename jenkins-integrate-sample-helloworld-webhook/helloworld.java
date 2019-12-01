206d399e190a97043ccc7306ce1b24eae0535b19
karsivakumar <sivakumar.karthik@gmail.com>
2019-11-30 18:39:58 -0600

    first comment

---
+++ jenkins-integrate-sample-helloworld-webhook/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld{
+public static void main(String[] args) {
+	System.out.println("----**** Hello and welcome to jenkins training! ****------");
+	System.out.println("----**** Added a GIT integration plug-in! ****------");
+}
+}
