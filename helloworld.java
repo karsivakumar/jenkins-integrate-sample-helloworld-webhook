d536d12998627168eb6205d3576db628314a861f
karsivakumar <sivakumar.karthik@gmail.com>
2019-11-30 21:51:10 -0600

    changes to helloworld 1

--- helloworld.java
+++ helloworld.java
@@ -2,5 +2,6 @@ public class helloworld{
 public static void main(String[] args) {
 	System.out.println("----**** Hello and welcome to jenkins training! ****------");
 	System.out.println("----**** Added a GIT webhook plug-in! ****------");
+	System.out.println("----**** Testing the GIT webhook plug-in! ****------");
 }
 }
